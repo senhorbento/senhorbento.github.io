eccb70297f0d3e20ce83dbc1cf573bb6116173fa
senhorbento <sbento.ti@gmail.com>
2024-02-11 17:36:05 -0300

    Fix: ets name

--- src/src/app/app.routes.ts
+++ src/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '**', component: HomeComponent },
+    { path: '', component: HomeComponent },
 ];
